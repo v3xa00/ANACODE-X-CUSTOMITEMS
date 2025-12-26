0dfa2f87c4881f5ef4a46c8bf5ca42dd5a1b6b63
v3xa00 <tomekgregori777@gmail.com>
2025-12-26 22:08:51 +0100

    Update TitleUtil.java

--- src/main/java/pl/anacode/customitems/util/TitleUtil.java
+++ src/main/java/pl/anacode/customitems/util/TitleUtil.java
@@ -9,12 +9,18 @@ import java.time.Duration;
 public class TitleUtil {
 
     public static void sendSubtitle(Player player, String subtitle) {
-        player.showTitle(Title.title(Component.empty(), ColorUtil.component(subtitle),
-                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(1400), Duration.ofMillis(400))));
+        player.showTitle(Title.title(
+                Component.empty(),
+                ColorUtil.component(subtitle),
+                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(1400), Duration.ofMillis(400))
+        ));
     }
 
     public static void sendTitleSubtitle(Player player, String title, String subtitle) {
-        player.showTitle(Title.title(ColorUtil.component(title), ColorUtil.component(subtitle),
-                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(2000), Duration.ofMillis(400))));
+        player.showTitle(Title.title(
+                ColorUtil.component(title),
+                ColorUtil.component(subtitle),
+                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(2000), Duration.ofMillis(400))
+        ));
     }
 }
