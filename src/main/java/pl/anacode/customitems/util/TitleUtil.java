07472481580c39c78e2bcb9ca0b70e9f41811faf
v3xa00 <tomekgregori777@gmail.com>
2025-12-26 21:22:11 +0100

    Create TitleUtil.java

---
+++ src/main/java/pl/anacode/customitems/util/TitleUtil.java
@@ -0,0 +1,20 @@
+package pl.anacode.customitems.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
+
+import java.time.Duration;
+
+public class TitleUtil {
+
+    public static void sendSubtitle(Player player, String subtitle) {
+        player.showTitle(Title.title(Component.empty(), ColorUtil.component(subtitle),
+                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(1400), Duration.ofMillis(400))));
+    }
+
+    public static void sendTitleSubtitle(Player player, String title, String subtitle) {
+        player.showTitle(Title.title(ColorUtil.component(title), ColorUtil.component(subtitle),
+                Title.Times.times(Duration.ofMillis(200), Duration.ofMillis(2000), Duration.ofMillis(400))));
+    }
+}
