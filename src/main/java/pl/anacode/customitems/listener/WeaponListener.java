52760e3424a704af46f23215634879bd98bf679a
v3xa00 <tomekgregori777@gmail.com>
2025-12-26 22:09:55 +0100

    Update WeaponListener.java

--- src/main/java/pl/anacode/customitems/listener/WeaponListener.java
+++ src/main/java/pl/anacode/customitems/listener/WeaponListener.java
@@ -1,19 +1,17 @@
 package pl.anacode.customitems.listener;
 
-import org.bukkit.*;
-import org.bukkit.entity.*;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 import pl.anacode.customitems.AnacodeCustomItems;
 import pl.anacode.customitems.manager.CooldownManager;
-import pl.anacode.customitems.util.ColorUtil;
 import pl.anacode.customitems.util.TitleUtil;
 
 import java.util.HashMap;
@@ -37,12 +35,31 @@ public class WeaponListener implements Listener {
         ItemStack item = attacker.getInventory().getItemInMainHand();
         if (item == null || !item.hasItemMeta()) return;
 
-        String id = ItemBuilder.getCustomItemId(item); // teraz używa poprawnej metody
+        String id = plugin.itemManager.getCustomItemId(item);
         if (id == null) return;
 
         CooldownManager cd = plugin.cooldownManager;
 
         switch (id) {
+            case "siekiera_grincha" -> {
+                if (cd.isOnCooldown(attacker, "siekiera_grincha")) return;
+                victim.getWorld().strikeLightningEffect(victim.getLocation());
+                double dmg = victim.getHealth() * 0.30;
+                victim.damage(dmg, attacker);
+                TitleUtil.sendSubtitle(attacker, "&7Uderzyłeś gracza &f" + victim.getName() + " &asiekierą grincza!");
+                TitleUtil.sendSubtitle(victim, "&7Zostałeś uderzony &asiekierą grincza&7!");
+                cd.setCooldown(attacker, "siekiera_grincha", 60);
+            }
+
+            case "marchewkowy_miecz" -> {
+                if (cd.isOnCooldown(attacker, "marchewkowy_miecz")) return;
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 255));
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20, 128, false, false, false));
+                TitleUtil.sendSubtitle(attacker, "&7Zamroziłeś gracza &f" + victim.getName() + "&7!");
+                TitleUtil.sendSubtitle(victim, "&7Zostałeś &bzamrożony&7!");
+                cd.setCooldown(attacker, "marchewkowy_miecz", 60);
+            }
+
             case "lopata_grincha" -> {
                 if (cd.isOnCooldown(attacker, "lopata_grincha")) return;
                 Location loc = victim.getLocation();
@@ -50,14 +67,37 @@ public class WeaponListener implements Listener {
                 loc.setPitch((float) (Math.random() * 180 - 90));
                 victim.teleport(loc);
                 victim.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 4));
-                victim.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE, 20, 2));
                 victim.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 20, 1));
                 TitleUtil.sendSubtitle(attacker, "&7Zamroziłeś gracza &f" + victim.getName() + " &ałopatą grincza&7!");
                 TitleUtil.sendSubtitle(victim, "&7Zostałeś zamrożony &ałopatą grincza&7!");
                 cd.setCooldown(attacker, "lopata_grincha", 15);
             }
 
+            case "splesniala_kanapka" -> {
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 600, 0));
+                TitleUtil.sendSubtitle(attacker, "&7Zakaziłeś gracza &f" + victim.getName() + " &4spleśniałą kanapką&7!");
+                TitleUtil.sendSubtitle(victim, "&7Zostałeś zakażony &4spleśniałą kanapką&7!");
+                item.setAmount(item.getAmount() - 1);
+            }
+
+            case "zatruty_olowek" -> {
+                if (cd.isOnCooldown(attacker, "zatruty_olowek")) return;
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 300, 0));
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 300, 0));
+                TitleUtil.sendSubtitle(attacker, "&7Zatrułeś gracza &f" + victim.getName() + "&7!");
+                TitleUtil.sendSubtitle(victim, "&7Zostałeś zatruty &azakażonym ołówkiem&7!");
+                cd.setCooldown(attacker, "zatruty_olowek", 60);
+            }
+
+            case "zlamane_serce" -> {
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 200, 0));
+                TitleUtil.sendSubtitle(attacker, "&7Złamałeś &dserce &7gracza: &f" + victim.getName() + "&7!");
+                TitleUtil.sendSubtitle(victim, "&7Twoje &dserce &7zostało złamane!");
+                item.setAmount(item.getAmount() - 1);
+            }
+
             case "parawan" -> {
+                if (cd.isOnCooldown(attacker, "parawan")) return;
                 for (Player p : attacker.getLocation().getNearbyPlayers(5)) {
                     if (p == attacker) continue;
                     Vector dir = p.getLocation().toVector().subtract(attacker.getLocation().toVector()).normalize().multiply(3).setY(0.8);
@@ -68,9 +108,28 @@ public class WeaponListener implements Listener {
                 }
                 TitleUtil.sendSubtitle(attacker, "&7Odrzuciłeś wszystkich graczy w pobliżu!");
                 item.setAmount(item.getAmount() - 1);
+                cd.setCooldown(attacker, "parawan", 15);
             }
 
-            // ... reszta itemów taka sama jak wcześniej
+            case "boski_topor" -> {
+                if (cd.isOnCooldown(attacker, "boski_topor")) return;
+                attacker.getWorld().strikeLightningEffect(attacker.getLocation());
+                attacker.getWorld().playSound(attacker.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_IMPACT, 2f, 0.8f);
+                for (Player p : attacker.getLocation().getNearbyPlayers(4)) {
+                    if (p == attacker) continue;
+                    Vector dir = p.getLocation().toVector().subtract(attacker.getLocation().toVector()).normalize().multiply(2.5).setY(0.9);
+                    p.setVelocity(dir);
+                    TitleUtil.sendSubtitle(p, "&7Gracz &f" + attacker.getName() + " &7użył &bboski topór&7!");
+                }
+                TitleUtil.sendSubtitle(attacker, "&7Aktywowałeś &bboski topór&7!");
+                cd.setCooldown(attacker, "boski_topor", 60);
+            }
+
+            case "zajeczy_miecz" -> {
+                if (cd.isOnCooldown(attacker, "zajeczy_miecz")) return;
+                victim.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 80, 128, false, false, false));
+                cd.setCooldown(attacker, "zajeczy_miecz", 60);
+            }
         }
     }
 }
