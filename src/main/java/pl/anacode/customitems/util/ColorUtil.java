3475b75ebfc0882d7767ca9b7961996ca3218dee
v3xa00 <tomekgregori777@gmail.com>
2025-12-26 21:21:50 +0100

    Create ColorUtil.java

---
+++ src/main/java/pl/anacode/customitems/util/ColorUtil.java
@@ -0,0 +1,16 @@
+package pl.anacode.customitems.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class ColorUtil {
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+
+    public static String color(String text) {
+        return text.replace("&", "§");
+    }
+
+    public static Component component(String text) {
+        return LEGACY.deserialize(text);
+    }
+}
